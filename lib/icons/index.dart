8ad45df87503d181bda5ffcdc007c099a93c6645
volcan0 <east.qrj@gmail.com>
2022-10-16 14:49:14 +0800

    chore(): add iconfont assest

---
+++ lib/icons/index.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/widgets.dart';
+
+class IconFontIcons {
+  static const IconData iconArrowLeft =
+      IconData(0xe744, fontFamily: 'IconFont');
+
+  static const IconData iconArrowLeft1 =
+      IconData(0xe755, fontFamily: 'IconFont');
+
+  static const IconData iconArrowRight =
+      IconData(0xe743, fontFamily: 'IconFont');
+
+  static const IconData iconArrowRight1 =
+      IconData(0xe761, fontFamily: 'IconFont');
+
+  static const IconData iconActivity = IconData(0xe600, fontFamily: 'IconFont');
+
+  // 优惠券
+  static const IconData iconCoupon = IconData(0xe65a, fontFamily: 'IconFont');
+
+  static const IconData iconDraft = IconData(0xe605, fontFamily: 'IconFont');
+  static const IconData iconReadRecord =
+      IconData(0xe62f, fontFamily: 'IconFont');
+
+  static const IconData iconFeedback = IconData(0xe617, fontFamily: 'IconFont');
+
+  static const IconData iconResume = IconData(0xe616, fontFamily: 'IconFont');
+
+  // 我的圈子
+  static const IconData iconMyGroup = IconData(0xe603, fontFamily: 'IconFont');
+
+  // 内容管理
+  static const IconData iconContent = IconData(0xe604, fontFamily: 'IconFont');
+
+  // 推广
+  static const IconData iconPromotion =
+      IconData(0xe604, fontFamily: 'IconFont');
+  // 标签管理
+  static const IconData iconTagManage =
+      IconData(0xe7a2, fontFamily: 'IconFont');
+  // 报名
+  static const IconData iconRegister = IconData(0xe632, fontFamily: 'IconFont');
+  // 文章管理
+  static const IconData iconArticle = IconData(0xe6a6, fontFamily: 'IconFont');
+  // 文章管理
+  static const IconData iconNotes = IconData(0xe6f3, fontFamily: 'IconFont');
+  // 数据管理
+  static const IconData iconDatas = IconData(0xe8b9, fontFamily: 'IconFont');
+}
